18e2d4ab78ce9a29c5c9a0ac9a4226af5481a60b
MunnaKhan99 <munnakhan01329@gmail.com>
2025-09-19 00:42:57 +0600

    add toastify and search functionality

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -21,9 +21,19 @@ const ReadList = () => {
         setReadList(myReadList);
     }, [])
 
-    const handleSort = (type) => (
+    const handleSort = (type) => {
         setSort(type)
-    )
+        if (type === "pages"){
+            const sortedByPage = [...readList].sort((a,b)=>a.totalPages - b.totalPages);
+            setReadList(sortedByPage);
+            console.log(sortedByPage);
+        }
+        if (type === "ratings"){
+            const sortedByRatings = [...readList].sort((a,b)=>a.rating - b.rating);
+            setReadList(sortedByRatings);
+            console.log(sortedByRatings);
+        }
+    }
 
     return (
         <div>
